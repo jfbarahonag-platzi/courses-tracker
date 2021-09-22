adac1404d86e01f97401b65228e56af273af0005
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-09-22 00:21:51 -0500

    add a new course functionality was started to be implemented

--- app.js
+++ app.js
@@ -16,8 +16,73 @@ new Vue({
     computed: {},
 
     methods: {
+
+        courseAlreadyExists(course) {
+            //list empty
+            if (this.courses.length == 0) {
+                return false
+            }
+
+            //check the list
+            for (const c of this.courses) {
+                // Compare the titles
+                if (c.title === course.title) {
+                    return true
+                }
+            }
+
+            return false
+        },
+
+        modifyCourseTime(course) {
+            for (const c of this.courses) {
+                if (c.title === course.title) {
+                    
+                    c.time = parseInt(c.time, 10) + parseInt(course.time, 10)
+                    console.log(c);
+                    break
+                }
+            }
+        },
+
+        cleanInputFields() {
+            this.title = ''
+            this.time = ''
+        },
+
+        validateHours() {
+            return this.time > 0 
+        },
+
+        validateTitle() {
+            return this.title !== ''
+        },
+
         addCourse() {
-            
+            if (!this.validateHours() || !this.validateTitle()) {
+                console.log("At least one param is not valid")
+                this.cleanInputFields()
+                return
+            }
+
+            const courseObj = {title: this.title, time: parseInt(this.time, 10)}
+
+            console.log(courseObj)
+
+            if (!this.courseAlreadyExists(courseObj)) {
+                // Add the course to the list
+                this.courses.push(courseObj)
+                console.log("saved");
+            }
+            else {
+                // Increment the hours qty
+                this.modifyCourseTime(courseObj)
+            }
+
+            this.totalTime += parseInt(this.time, 10)
+
+            this.cleanInputFields()
+
         }
     }
 })
